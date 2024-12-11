cb2b5c2afa3b18a463a9803a3f72eb188030f96c
2xmxu <leeeun099@naver.com>
2024-12-11 16:16:34 +0900

    aa

--- tj2024b_backend/src/day03/Example4.java
+++ tj2024b_backend/src/day03/Example4.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Example4 {
 
 	public static void main(String[] args) {
-		// 문제1 : 점수를 정수형으로 입력받아 점수가 90점 이상이면 '합격' 아니면 '불합격' 출력 하시오.
+		// 문제1 : 점수를 정수형으로 입력받아 점수가 90점 이상이면 '합격' 아니면 '불합격' 출력 하시오.*/
 		Scanner sc = new Scanner(System.in);
-		/*System.out.println("성적을 입력하세요");
+		System.out.println("성적을 입력하세요");
 		int score = sc.nextInt();
 		
 		if(score>=90) {
@@ -46,10 +46,10 @@ public class Example4 {
 		}else {
 			System.out.println(k);
 		} 
-		*/
+		
 		// 문제4 : 각 3개의 정수형으로 수를 입력받아 오름차순 순서대로 출력하시오. [ 전제조건 : 각 정수는 서로 다른 정수값 ]
 		
-		/*
+		
 		String strResultABC;
 		
 		// 2. 사용자로부터 입력받기
@@ -61,7 +61,6 @@ public class Example4 {
 		
 		System.out.print("세 번째 정수 입력 : ");
 		String strC = sc.next();
-		
 		
 		int nA = Integer.parseInt(strA);
 		int nB = Integer.parseInt(strB);
@@ -106,8 +105,7 @@ public class Example4 {
 			}
 		}
 		
-		// 5. 결과값 출력
-		System.out.printf("정렬 결과 : %s", strResultABC); 
+		System.out.printf("정렬 결과 : %s", strResultABC);  /*
 		
 	
 
@@ -115,7 +113,7 @@ public class Example4 {
 	      - 가위가 '0' 이고 바위가 '1' 이고 보가 '2' 일때 플레이어1와 플레이어2 가 있습니다.
 	      - 승리자 판단과 무승부 경우의수를 출력하시오.
 	       [입력 조건] 플레이어1 과 플레이어2 에게 각각 입력을 받습니다.
-	      [출력 조건] 플레이어1 이기면 ' 플레이어1 승리 ', 플레이어2 이기면 ' 플레이어2 승리 ' 무승부 이면 '무승부' 출력 하시오.
+	      [출력 조건] 플레이어1 이기면 ' 플레이어1 승리 ', 플레이어2 이기면 ' 플레이어2 승리 ' 무승부 이면 '무승부' 출력 하시오.*/
 		 int scissors = 0;
 		 int rock = 1;
 		 int paper = 2;
@@ -132,7 +130,7 @@ public class Example4 {
 			}else {
 				System.out.println("플레이어2 승리");
 			}
-		 */
+		 
 		
 		/* 문제6 : 윤년/평년 판단하기
    		   [ 입력조건 ] 하나의 연도를 입력 받습니다.
@@ -140,7 +138,7 @@ public class Example4 {
 		   -연수가 4로 나누어 떨어지는 해는 윤년으로 한다.
 		   -연수가 4, 100으로 나누어 떨어지는 해는 평년으로 한다.
 		   -연수가 4, 100, 400으로 나누어 떨어지는 해는 윤년으로 둔다.
-		   [ 출력조건] '입력한OOOO년 은' 윤년 입니다.  혹은 평년 입니다 로 출력하시오.
+		   [ 출력조건] '입력한OOOO년 은' 윤년 입니다.  혹은 평년 입니다 로 출력하시오.*/
 				 
 			System.out.println("연도를 입력해주세요");
 			int year = sc.nextInt();
@@ -150,14 +148,15 @@ public class Example4 {
 			}else {
 				System.out.println("윤년");
 			}
-			*/
+			
 			
 		/*문제7 : 주차 사용요금 계산하기
    [ 선언 변수 조건 ] 입차시간이 9시30분 입니다. 
    - int 시 = 9;  int 분 = 30; 이와 같이 변수를 먼저 선언해주세요.
    [ 출차시간 입력 조건 ] 출차시간의 시 와 분을 각 입력받아 각 변수에 저장하시오. 
    [ 처리출력 조건 ] 1분당 1000원으로 계산하여 사용요금' 0000원' 을 출력하시오,
-   - 단 입차 시간 이전 으로 입력 할 경우에는 '출차시간 오류' 출력하시오.
+   - 단 입차 시간 이전 으로 입력 할 경우에는 '출차시간 오류' 출력하시오. */
+		
 			int 시 = 9;  int 분 = 30;
 			System.out.println("출차시간의 시를 입력해주세요");
 			int outH = sc.nextInt();
@@ -170,14 +169,12 @@ public class Example4 {
 			
 			int money = outMM * 1000;
 			
-			
-			
 			if(outMM<start) {
 				System.out.println("출차시간 오류");
 			}else {
 				System.out.println(money);
 
-			} */
+			} 
 			
 		
 		/*문제8 : 로그인 처리
@@ -190,9 +187,11 @@ public class Example4 {
    1. 문자열 비교시에는 == 같다 연산자 대신에 .equals() 함수를 이용해주세요.
    예] 
       String name = scan.next();
-      name =="admin" 대신에 name.equals( "admin" ) 사용.
+      name =="admin" 대신에 name.equals( "admin" ) 사용.*/
+		
 		String name = "admin";
 		String pw = "1234";
+		
 		
 		System.out.println("ID를 입력하세요");
 		String name1 = sc.next();
@@ -206,7 +205,7 @@ public class Example4 {
 		}else if(name.equals( name1 ) && pw != pw1){
 			System.out.println("비밀번호 정보가 일치하지 않습니다.");
 		}
-		*/
+		
 		/*문제9 : 당첨번호 개수  찾기
    [ 선언 변수 조건 ] int 공1 = 14 ; int 공2 = 21 ; int 공3 = 9; 이와 같이 변수를 먼저 선언해주세요.
    [ 입력 조건 ] 세개의 정수를 입력받아 각 변수에 저장하시오.
