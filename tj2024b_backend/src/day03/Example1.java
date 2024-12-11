ee757c3293dd8825eef0863689b3a0df00e0da75
2xmxu <leeeun099@naver.com>
2024-12-11 10:23:15 +0900

    day03

---
+++ tj2024b_backend/src/day03/Example1.java
@@ -0,0 +1,30 @@
+package day03;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int age = 24;
+		
+		int mathScore = 90;
+		int engScore = 70;
+		int korScore = 70;
+		int totalScore = mathScore + engScore + korScore;
+		System.out.println(totalScore);
+		
+		double avgScore = totalScore/3.0;
+		System.out.println(avgScore);
+		
+		int num = 10;
+		
+		System.out.println(+num);
+		System.out.println(-num);
+		System.out.println(num);
+		
+		num = -num;
+		System.out.println(num);
+		
+		
+
+	}
+
+}
