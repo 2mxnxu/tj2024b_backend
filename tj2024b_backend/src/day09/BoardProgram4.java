561992c3d60bcfebe2a4c81db7d7bc0b26d3d7d8
2mxnxu <leeeun099@naver.com>
2024-12-19 10:59:54 +0900

    day09

--- tj2024b_backend/src/day09/BoardProgram4.java
+++ tj2024b_backend/src/day09/BoardProgram4.java
@@ -7,15 +7,16 @@ public class BoardProgram4 {
 	public static void main(String[] args) {
 		
 		Boardservice bs = new Boardservice();
-		
+		// 객체 여러개를 저장하는 배열 생성
+		Board[] boards = new Board[100];
 		while(true) {	
 		System.out.println("1.등록 2.출력 : ");
 		Scanner sc = new Scanner(System.in);
 		int choose = sc.nextInt();
 		if(choose == 1) {
-			bs.boardAdd();
+			bs.boardAdd(sc, boards);
 		}else if(choose == 2) {
-			bs.boardPrint();
+			bs.boardPrint(boards);
 		}
 
 	}
