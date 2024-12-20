42981d33980d59385889a88ad54d935d97e0431b
2mxnxu <leeeun099@naver.com>
2024-12-20 12:10:55 +0900

    day10

---
+++ tj2024b_backend/src/day10/BoardDTO.java
@@ -0,0 +1,7 @@
+package day10;
+
+public class BoardDTO {
+	// D(data)T(transfer)O(object) : 데이터 이동 객체
+	// 여러 사람들이 작업할때 클래스명에 공통적으로 dto 명시 함으로써 클래스 사용처 파악이 쉽다.
+		// 추후에 dto, vo, dao, controller, service, view, entity
+}
