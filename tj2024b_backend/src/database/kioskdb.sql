a900a81d88cad8ac5a2a843d6bc8f582ae0bcc07
2xmxu <leeeun099@naver.com>
2024-12-31 18:09:51 +0900

    day16

--- tj2024b_backend/src/database/kioskdb.sql
+++ tj2024b_backend/src/database/kioskdb.sql
@@ -1,7 +1,7 @@
 create database kiosk;
 use kiosk;
 create table kioskmenu(
-	kno int not null, #카테고리 식별코드
+	kno int auto_increment, #카테고리 식별코드
     kname varchar(10) not null, #카테고리 이름 
     constraint primary key(kno)
 );
@@ -9,11 +9,11 @@ create table kioskmenu(
 create table kioskproduct(
 	pno int not null unique, #제품번호
     pname varchar(10) not null, #제품이름
-    pcount smallint not null, #제품수량
-    pprice int not null, #제품가격
+    pprice int unsigned default 0, #제품가격
     kno int,
-    constraint foreign key (pno) references kioskmenu (kno),
-    constraint primary key(pname)
+    constraint primary key(pno),
+    kno int,
+    constraint foreign key(kno) references kioskmenu (kno)
 );
 
 create table kioskorder(
@@ -21,8 +21,20 @@ create table kioskorder(
     odate datetime default now(), #주문날짜
     constraint primary key(ono)
 );
+
+create table orderdetail(
+	odno int auto_increment,	#주문상세
+    odamount tinyint unsigned not null,		#주문수량
+    constraint primary key(odno),
+    pno int,
+    constraint foreign key(pno) references kioskproduct (pno),
+    ono int,
+    constraint foreign key(ono) references kioskproduct (ono)
+);
+
 show tables;
 select * from kioskmenu;
 select * from kioskproduct;
 select * from kioskorder;
+select * from orderdetail;
 
