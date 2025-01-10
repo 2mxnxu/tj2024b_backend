a0cdf6801c8fa17f615766ed55c0f98c8cd9463f
2mxnxu <leeeun099@naver.com>
2025-01-10 12:37:06 +0900

    2025.01.10

--- tj2024b_backend/src/boardservice10/model/dao/BoardDao.java
+++ tj2024b_backend/src/boardservice10/model/dao/BoardDao.java
@@ -18,7 +18,9 @@ public class BoardDao extends Dao {
 		ArrayList<BoardDto> list = new ArrayList<BoardDto>();
 		// 조회된 레코드 하나씩 저장하여 여러개 레코드를 저장하는 리스트 객체
 		try {
-			String sql = "select * from board";
+			String sql = "select b.* , c.cname , m.mid "
+					+ " from board as b inner join category as c on b.cno = c.cno "
+					+ " inner join member as m on b.mno = m.mno";
 			PreparedStatement ps;
 			ps = conn.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
@@ -32,6 +34,9 @@ public class BoardDao extends Dao {
 				int cno = rs.getInt("cno");
 				BoardDto boardDto = new BoardDto(bno, btitle, bcontent, bview, bdate, mno, cno);
 				// 생성된 객체를 리스트에 담기
+				boardDto.setCname(rs.getString("cname"));
+				boardDto.setCname(rs.getString("mid"));
+				
 				list.add(boardDto);
 			}
 		} catch (SQLException e) {
@@ -42,19 +47,22 @@ public class BoardDao extends Dao {
 	// 개별출력 select * from board where bno =?;
 	public BoardDto findById(int bno) {
 		try {
-			String sql = "select * from board where bno =?";
+			String sql = "select b.* , c.cname , m.mid "
+					+ " from board as b inner join category as c on b.cno = c.cno "
+					+ " inner join member as m on b.mno = m.mno "
+					+ " where b.bno = ? ";
 			PreparedStatement ps = conn.prepareStatement(sql);
 			ps.setInt(1, bno);
 			ResultSet rs = ps.executeQuery();
 			if(rs.next()) {
-				int bno1 = rs.getInt("bno");
-				String btitle = rs.getString("btitle");
-				String bcontent = rs.getString("bcontent");
-				int bview = rs.getInt("bview");
-				String bdate = rs.getString("bdate");
-				int mno = rs.getInt("mno");
-				int cno = rs.getInt("cno");
-				BoardDto boardDto = new BoardDto(bno1, btitle, bcontent, bview, bdate, mno, cno);
+				BoardDto boardDto = new BoardDto(
+						rs.getInt("bno") , rs.getString("btitle") , 
+						rs.getString("bcontent") , rs.getInt("bview") ,
+						rs.getString("bdate") , rs.getInt("mno"),
+						rs.getInt("cno" ));
+				boardDto.setCname( rs.getString("cname" ) );
+				boardDto.setMid( rs.getString("mid") );
+				
 				return boardDto;
 			}
 		}catch(SQLException e) {
--- tj2024b_backend/src/boardservice10/model/dto/BoardDto.java
+++ tj2024b_backend/src/boardservice10/model/dto/BoardDto.java
@@ -9,6 +9,8 @@ public class BoardDto {
 	private String bdate;
 	private int mno;			// 작성자 회원번호
 	private int cno;			// 카테고리 번호
+	private String mid;
+	private String cname;
 	// 작성자 회원번호가 아닌 작성자의 아이디	FK 값 이용하여 PK 레코드의 정보 얻기
  	// 카테고리 번호가 아닌 카테고리의 이름 
 	// 2. 생성자
@@ -29,7 +31,6 @@ public class BoardDto {
 		return "BoardDto [bno=" + bno + ", btitle=" + btitle + ", bcontent=" + bcontent + ", bview=" + bview
 				+ ", bdate=" + bdate + ", mno=" + mno + ", cno=" + cno + "]";
 	}
-	
 	public int getBno() {
 		return bno;
 	}
@@ -72,5 +73,18 @@ public class BoardDto {
 	public void setCno(int cno) {
 		this.cno = cno;
 	}
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
 	
 }
